ae6592cb55069c8a0ad5fe26575086f411bb2005
Jodavaga <jodavaga@gmail.com>
2018-05-09 17:31:15 -0500

    Base project was updated, added styles

--- src/app/shared-components/sidebar/sidebar.component.ts
+++ src/app/shared-components/sidebar/sidebar.component.ts
@@ -3,13 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [`
-
-  #menu{
-    border-right: 1px solid lightgrey;
-  }
-
-  `]
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
