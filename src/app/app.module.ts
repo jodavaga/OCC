2289e52506112c66633102a29e38ca9bf0dc0586
Jodavaga <jodavaga@gmail.com>
2018-05-09 18:18:39 -0500

    First two components created, breadcrump, home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared-components/navbar/navbar.component';
 import { SidebarComponent } from './shared-components/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared-components/breadcrumb/breadcrumb.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    BreadcrumbComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
